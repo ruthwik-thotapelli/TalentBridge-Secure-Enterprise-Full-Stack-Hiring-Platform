93b53d4f9a589369a3a8c49e083a51a58d95053a
[200~@Ruthwikreddyladdu <ruthwikreddyladdu@gmail.com>
2026-03-04 16:55:40 +0530

    connect railway backend api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://talentbridge-secure-enterprise-full-stack-hiring-production.up.railway.app",
+  withCredentials: true
 });
 
 // auto attach token
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
